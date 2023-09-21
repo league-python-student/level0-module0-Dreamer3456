6b6c7a41f669bac86482eb4416130daec8a8517b
root <hello>
2023-09-21 01:26:02 +0000

    shapes and colors tashe me logical turtle

--- _01_drawing/_a_shapes_and_colors.py
+++ _01_drawing/_a_shapes_and_colors.py
@@ -8,27 +8,47 @@ if __name__ == '__main__':
     my_turtle = turtle.Turtle()
 
     # Make your turtle's shape 'turtle', .shape('turtle')
-
+    my_turtle.shape('turtle')
     # Set your turtle's speed using .speed(2)
-
+    my_turtle.speed(2)
     # Set your turtle's color using .color('green') and .pencolor('blue')
-
+    my_turtle.color('green')
+    my_turtle.pencolor('blue')
     # Move your turtle forward using .forward(100)
     # TEST    Did your turtle move forward?
-
-    # Move your turtle left or right using .left(90) or .right(90)
-
+    my_turtle.begin_fill()
+    for i in range(4):
+     my_turtle.forward(100)
+     my_turtle.left(90)
+    my_turtle.end_fill()
     # Now put the forward and left/right code into a for loop to repeat 4 times.
     # TEST    Did your turtle draw a square?
 
     # Move your turtle to a new place on the screen using .goto(x, y)
     # x=0 and y=0 is the center of the screen
-
+    my_turtle.goto(x=100, y=-250)
     # Have your turtle draw a circle using .circle(radius, steps=30)
     # TEST    Did your turtle draw a circle?
+    my_turtle.begin_fill()
+    my_turtle.circle(radius=50, steps=30)
+    my_turtle.end_fill()
+
+    my_turtle.goto(x=-250, y=250)
+
+    my_turtle.begin_fill()
+    my_turtle.circle(radius=60, steps=30)
+    my_turtle.end_fill()
+
+    my_turtle.goto(x=250, y=250)
+
+    my_turtle.begin_fill()
+    my_turtle.circle(radius=40, steps=40)
+    my_turtle.end_fill()
+
+
 
     # Add color to your shape by adding .begin_fill() before drawing the circle
-    # and .end_fill() below
+    # and my_turtle.end_fill() below
 
     # Draw 3 more shapes with different fill colors!
 
--- _01_drawing/_c_logical_turtle.py
+++ _01_drawing/_c_logical_turtle.py
@@ -13,15 +13,19 @@ def screen_clicked(x, y):
     print('You pressed: x=' + str(x) + ', y=' + str(y))
     
     # 6. Call the turtle .penup() method
-    
+    my_turtle.penup()
     # 7. Move the turtle to a new location using .goto(x, y)
-
+    my_turtle.goto(x=200, y=200)
 
 def turtle_clicked(x, y):
     print('turtle clicked!')
     
     # 8. Make a for loop to run the next instructions 3 times
-        
+    for i in range(3):
+     my_turtle.right(120)
+    my_turtle.color(get_random_color())
+
+
         # 9. Make the turtle spin 360 degrees using the .right() method
         
         # 10. Use the .color() method and getRandomColor() function to change
@@ -34,16 +38,17 @@ if __name__ == '__main__':
     window.setup(width=0.75, height=0.8, startx=0, starty=0)
     
     # 1. Make a new turtle
-    
+    my_turtle = turtle.Turtle()
     # 2. Make your turtle's shape 'turtle', .shape('turtle')
-    
+    my_turtle.shape('turtle')
     # 3. Set your turtle's color using .color('green') and .pencolor('blue')
-    
+    my_turtle.color('green')
+    my_turtle.pencolor('blue')
     # 4. Set and new width, length, and outline of our turtle
     #    my_turtle.turtlesize(stretch_wid=10, stretch_len=10, outline=4)
-
+    my_turtle.turtlesize(stretch_wid=10, stretch_len=10, outline=4)
     # 5. Uncomment the following line and replace 'my_turtle' with your turtle
-    # my_turtle.onclick(turtle_clicked)
+    my_turtle.onclick(turtle_clicked)
 
 # ===================== DO NOT EDIT THE CODE BELOW ============================
     window.onclick(screen_clicked)
